38db21d4b1612e0e5a9b1b845d1f3a21ffc6da80
oksanakney <oksanakney@gmail.com>
2024-03-12 19:10:18 +0200

    Add project files.

---
+++ NebulaNewsSystem.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NebulaNewsSystem.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
