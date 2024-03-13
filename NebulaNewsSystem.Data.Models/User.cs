affab3d4d28bd3007ee90c30e2b009e6798266e3
oksanakney <oksanakney@gmail.com>
2024-03-13 15:05:30 +0200

    Problems with Custom Identity Settings. Trying to update db without custom identity,  changes discarded

--- NebulaNewsSystem.Data.Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace NebulaNewsSystem.Data.Models
-{
-    public class User : IdentityUser<Guid>
-    {
-
-    }
-}
---
+++ NebulaNewsSystem.Data/NebulaNewsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace NebulaNewsSystem.Web.Data
+{
+    public class NebulaNewsDbContext : IdentityDbContext
+    {
+        public NebulaNewsDbContext(DbContextOptions<NebulaNewsDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
