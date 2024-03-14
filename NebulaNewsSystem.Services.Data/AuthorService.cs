1c5c1d72a6d5c22d512f1a72fb0f16dfa13733f9
oksanakney <oksanakney@gmail.com>
2024-03-14 21:12:53 +0200

    AuthorService and IAuthorService added in order to add action Become Author

---
+++ NebulaNewsSystem.Services.Data/AuthorService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NebulaNewsSystem.Services.Data.Interfaces;
+using NebulaNewsSystem.Web.Data;
+
+namespace NebulaNewsSystem.Services.Data
+{
+    public class AuthorService : IAuthorService
+    {
+        private readonly NebulaNewsDbContext dbContext;
+        public AuthorService(NebulaNewsDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async Task<bool> AuthorExistsByReaderIdAsync(string userId)
+        {
+            bool result = await this.dbContext
+                .Authors
+                .AnyAsync(au => au.Id.ToString() == userId);
+
+            return result;
+        }
+    }
+}
---
+++ NebulaNewsSystem.Services.Data/Interfaces/IAuthorService.cs
@@ -0,0 +1,7 @@
+﻿namespace NebulaNewsSystem.Services.Data.Interfaces
+{
+    public interface IAuthorService
+    {
+        Task<bool> AuthorExistsByReaderIdAsync(string userId);
+    }
+}
