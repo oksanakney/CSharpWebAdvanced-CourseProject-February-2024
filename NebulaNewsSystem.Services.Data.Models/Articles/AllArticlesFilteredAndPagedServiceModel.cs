e62639b79cbf1d2cf6f6faa671d1e1b76cd07d41
oksanakney <oksanakney@gmail.com>
2024-03-21 17:03:28 +0200

    View for sorting and Pagination Added

---
+++ NebulaNewsSystem.Services.Data.Models/Articles/AllArticlesFilteredAndPagedServiceModel.cs
@@ -0,0 +1,14 @@
+﻿using NebulaNewsSystem.Web.ViewModels.Article;
+
+namespace NebulaNewsSystem.Services.Data.Models.Articles
+{
+    public class AllArticlesFilteredAndPagedServiceModel
+    {
+        public AllArticlesFilteredAndPagedServiceModel()
+        {
+            this.Articles = new HashSet<ArticleAllViewModel>();
+        }
+        public int TotalArticlesCount { get; set; }
+        public IEnumerable<ArticleAllViewModel> Articles { get; set; }
+    }
+}
